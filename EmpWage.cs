9991c1d00c9d7da51adf74f60b20774e5e01eebb
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-16 13:29:53 +0530

    welcome

--- EmpWage.cs
+++ EmpWage.cs
@@ -11,18 +11,24 @@ namespace EmpWage
         public static void DialyEmpWage()
         {
             int FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_WAGE_PER_HOUR = 20;
 
             int emphrs = 0;
             int empwage = 0;
 
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
 
             if (empcheck == FULL_TIME)
             {
-                Console.WriteLine("employee is Present");
+                Console.WriteLine("employee is Present FULL_TIME");
                 emphrs = 8;
+            }
+            else if(empcheck==PART_TIME)
+            {
+                Console.WriteLine("Employee is Present for PART_TIME");
+                emphrs = 4;
             }
             else
             {
