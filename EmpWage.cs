245fe4acc600c3cd438289a85e1c902d279a0e8e
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-16 12:38:01 +0530

    welcome

---
+++ EmpWage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class EmpWage
+    {
+        public static void Empcheck()
+        {
+            Random random = new Random();
+            int x = random.Next(0, 2);
+            if (x == 0)
+            {
+                Console.WriteLine("employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+        }
+    }
+}
