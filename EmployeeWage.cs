71f2171b3f06933d4bae81e1f4adc39deaeffac7
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-16 14:56:50 +0530

    welcome

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -11,33 +11,41 @@ namespace EmpWage
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         public const int EMP_WAGE_PER_HOUR = 20;
+        public static int TOTAL_WORKING_DAYS = 20;
         public static void EmployeeWagecheck()
         {
             int emphrs = 0;
             int empwage = 0;
+            int totalempwage = 0;
 
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-
-            switch (empcheck)
+            for (int day = 0; day <= TOTAL_WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Employee is Present FULL_TIME");
-                    emphrs = 8;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
 
-                case PART_TIME:
-                    Console.WriteLine("Employee is Present PART_TIME");
-                    emphrs = 4;
-                    break;
+                switch (empcheck)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Employee is Present FULL_TIME");
+                        emphrs = 8;
+                        break;
 
-                default :
-                    Console.WriteLine("Employee is Absent");
-                    emphrs = 0;
-                    break;
+                    case PART_TIME:
+                        Console.WriteLine("Employee is Present PART_TIME");
+                        emphrs = 4;
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        emphrs = 0;
+                        break;
+                }
+                empwage = emphrs * EMP_WAGE_PER_HOUR;
+                totalempwage += empwage;
+                Console.WriteLine("Emp wage : " + empwage);
             }
-            empwage = emphrs * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Emp wage : " + empwage);
+
+            Console.WriteLine("Total Emp wage : " + totalempwage);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmpWage");
-            EmpWage.DialyEmpWage();
+            EmployeeWage.EmployeeWagecheck();
             Console.ReadLine();
         }
     }
