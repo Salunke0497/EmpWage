784fbc49e6ad5bd4b6c6ae3f43ca04b7bd4cb99c
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-27 14:05:38 +0530

    welcome

--- empwageclass.cs
+++ empwageclass.cs
@@ -9,18 +9,16 @@ namespace EmpWage
     internal class empwageclass
     {
         public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int ComputeEmpWage()
+        public const int IS_FULL_TIME = 2;        
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-
             //variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            //computation
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -40,8 +38,8 @@ namespace EmpWage
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Days#: " + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage: " +company + "is : " + totalEmpWage);
             return totalEmpWage;
         }
     }
