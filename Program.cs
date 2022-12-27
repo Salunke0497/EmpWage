b845fecca73b4b8f430f9903e1ce2d56aae1bc60
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-27 17:53:15 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeWageUC10
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("total wage of company is" + empWageBuilder.getTotalEmpWage(1));
+            Console.ReadLine();
+        }
+    }
+}
+           
